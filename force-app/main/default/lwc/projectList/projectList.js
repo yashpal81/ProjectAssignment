49f37f6f6a3f584ed13741562ae8a497f1a6805e
yashpal81 <sh.yashpal@gmail.com>
2025-08-17 11:12:55 +0530

    Create from Template

--- force-app/main/default/lwc/projectList/projectList.js
+++ force-app/main/default/lwc/projectList/projectList.js
@@ -1,5 +1,8 @@
 import { LightningElement, wire } from 'lwc';
 import getUserProjects from '@salesforce/apex/ProjectTaskWithSharingController.getUserProjects';
+import getTemplateProjects from '@salesforce/apex/ProjectTaskWithoutSharingController.getTemplateProjects';
+import startProjectCopyProcess from '@salesforce/apex/ProjectTaskWithoutSharingController.startProjectCopyProcess';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ProjectList extends LightningElement {
     projects;
@@ -14,6 +17,28 @@ export default class ProjectList extends LightningElement {
         { label: 'Is Template', fieldName: 'isTemplate__c', type: 'boolean' }
     ];
 
+    isTemplateModalOpen = false;
+    templateProjects = [];
+    templateColumns = [
+        { label: 'Name', fieldName: 'Name' },
+        { label: 'No. of Tasks', fieldName: 'TaskCount', type: 'number' },
+        { label: 'Created By', fieldName: 'CreatedByName' }
+    ];
+    selectedTemplateId = null;
+    isCreateDisabled = true;
+    newProjectName = '';
+    newStartDate = '';
+    newEndDate = '';
+    handleNameChange(event) {
+        this.newProjectName = event.target.value;
+    }
+    handleStartDateChange(event) {
+        this.newStartDate = event.target.value;
+    }
+    handleEndDateChange(event) {
+        this.newEndDate = event.target.value;
+    }
+
     @wire(getUserProjects)
     wiredProjects({ error, data }) {
         if (data) {
@@ -22,6 +47,67 @@ export default class ProjectList extends LightningElement {
         } else if (error) {
             this.error = error;
             this.projects = undefined;
+        }
+    }
+
+    handleOpenTemplateModal() {
+        this.isTemplateModalOpen = true;
+        this.loadTemplateProjects();
+    }
+    handleCloseTemplateModal() {
+        this.isTemplateModalOpen = false;
+        this.selectedTemplateId = null;
+        this.isCreateDisabled = true;
+    }
+    async loadTemplateProjects() {
+        try {
+            const result = await getTemplateProjects();
+            // Map wrapper fields to grid
+            this.templateProjects = result.map(tp => ({
+                Id: tp.id,
+                Name: tp.name,
+                TaskCount: tp.taskCount,
+                CreatedByName: tp.createdByName
+            }));
+        } catch (error) {
+            this.templateProjects = [];
+        }
+    }
+    handleTemplateSelection(event) {
+        const selectedRows = event.detail.selectedRows;
+        if (selectedRows.length === 1) {
+            this.selectedTemplateId = selectedRows[0].Id;
+            this.isCreateDisabled = false;
+        } else {
+            this.selectedTemplateId = null;
+            this.isCreateDisabled = true;
+        }
+    }
+    async handleCreateFromTemplate() {
+        if (!this.selectedTemplateId) return;
+        try {
+            await startProjectCopyProcess({
+                templateProjectId: this.selectedTemplateId,
+                newName: this.newProjectName,
+                newStartDate: this.newStartDate,
+                newEndDate: this.newEndDate
+            });
+            this.handleCloseTemplateModal();
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Project Creation Started',
+                    message: 'You will be notified when the project and its tasks are copied.',
+                    variant: 'info'
+                })
+            );
+        } catch (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: error.body ? error.body.message : error.message,
+                    variant: 'error'
+                })
+            );
         }
     }
 }
